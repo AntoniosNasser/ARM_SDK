ab8c0c541e5ea4eeb76140439a0a848144056f8d
AntoniosNasser <toosanasser12@gmail.com>
2020-12-23 14:22:33 +0200

    volatile added

--- system/01-MCAL/01-GPIO/GPIO_private.h
+++ system/01-MCAL/01-GPIO/GPIO_private.h
@@ -25,8 +25,8 @@
 // Port A
 #define GPIOA_CRL		*((uint32*)(GPIOA_BASE_ADDRESS))
 #define GPIOA_CRH		*((uint32*)(GPIOA_BASE_ADDRESS+0x04))
-#define GPIOA_IDR		*((uint32*)(GPIOA_BASE_ADDRESS+0x08))
-#define GPIOA_ODR   	*((uint32*)(GPIOA_BASE_ADDRESS+0x0C))
+#define GPIOA_IDR		*((volatile uint32*)(GPIOA_BASE_ADDRESS+0x08))
+#define GPIOA_ODR   	*((volatile uint32*)(GPIOA_BASE_ADDRESS+0x0C))
 #define GPIOA_BSRR  	*((uint32*)(GPIOA_BASE_ADDRESS+0x10))
 #define GPIOA_BRR		*((uint32*)(GPIOA_BASE_ADDRESS+0x14))
 #define GPIOA_LCKR		*((uint32*)(GPIOA_BASE_ADDRESS+0x18))
@@ -34,8 +34,8 @@
 // Port B
 #define GPIOB_CRL		*((uint32*)(GPIOB_BASE_ADDRESS))
 #define GPIOB_CRH		*((uint32*)(GPIOB_BASE_ADDRESS+0x04))
-#define GPIOB_IDR		*((uint32*)(GPIOB_BASE_ADDRESS+0x08))
-#define GPIOB_ODR   	*((uint32*)(GPIOB_BASE_ADDRESS+0x0C))
+#define GPIOB_IDR		*((volatile uint32*)(GPIOB_BASE_ADDRESS+0x08))
+#define GPIOB_ODR   	*((volatile uint32*)(GPIOB_BASE_ADDRESS+0x0C))
 #define GPIOB_BSRR  	*((uint32*)(GPIOB_BASE_ADDRESS+0x10))
 #define GPIOB_BRR		*((uint32*)(GPIOB_BASE_ADDRESS+0x14))
 #define GPIOB_LCKR		*((uint32*)(GPIOB_BASE_ADDRESS+0x18))
@@ -43,8 +43,8 @@
 // Port C
 #define GPIOC_CRL		*((uint32*)(GPIOC_BASE_ADDRESS))
 #define GPIOC_CRH		*((uint32*)(GPIOC_BASE_ADDRESS+0x04))
-#define GPIOC_IDR		*((uint32*)(GPIOC_BASE_ADDRESS+0x08))
-#define GPIOC_ODR   	*((uint32*)(GPIOC_BASE_ADDRESS+0x0C))
+#define GPIOC_IDR		*((volatile uint32*)(GPIOC_BASE_ADDRESS+0x08))
+#define GPIOC_ODR   	*((volatile uint32*)(GPIOC_BASE_ADDRESS+0x0C))
 #define GPIOC_BSRR  	*((uint32*)(GPIOC_BASE_ADDRESS+0x10))
 #define GPIOC_BRR		*((uint32*)(GPIOC_BASE_ADDRESS+0x14))
 #define GPIOC_LCKR		*((uint32*)(GPIOC_BASE_ADDRESS+0x18))
